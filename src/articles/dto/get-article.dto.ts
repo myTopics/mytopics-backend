d956169289c969e77eaa4b4da336ea6ff8f71a84
sebossert@students.zhaw.ch <sebossert@protonmail.com>
2019-09-28 09:58:55 +0200

    article stub, get-article dto

--- src/articles/dto/get-article.dto.ts
+++ src/articles/dto/get-article.dto.ts
@@ -1,1 +1,12 @@
-export class Article {}
+import { IsString } from 'class-validator';
+
+export class Article {
+    @IsString()
+    readonly title: string;
+    @IsString()
+    readonly summary: string;
+    @IsString()
+    readonly image: string;
+    @IsString()
+    readonly text: string;
+}
--- src/articles/interfaces/article.interface.ts
+++ src/articles/interfaces/article.interface.ts
@@ -1,1 +1,11 @@
-export interface Article {}
+export interface Article {
+    readonly title: string;
+    readonly summary: string;
+    readonly image: string;
+    readonly text: string;
+    /** Optional ? */
+    readonly author: string;
+    readonly source: string; // full url or only publication
+    readonly postedAt: Date;
+    readonly modifiedAt: Date;
+}
