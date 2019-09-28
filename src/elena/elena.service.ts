b634f82bc15c96f12a3ff50c71ec2d425c98a4e4
sebossert@students.zhaw.ch <sebossert@protonmail.com>
2019-09-28 23:05:16 +0200

    logs

--- src/elena/elena.service.ts
+++ src/elena/elena.service.ts
@@ -4,15 +4,11 @@ import { Article } from 'src/articles/interfaces/article.interface';
 @Injectable()
 export class ElenaService {
   async process(articles: Article[]): Promise<Article[]> {
-    const spawn = require('child_process').spawn;
     const promisses = [];
     articles.forEach(article => {
       const input = JSON.stringify(article);
       console.dir(input);
-      const process = spawn('python', [
-        'src/articles/news.py',
-        input,
-      ]);
+      const process = require('child_process').execScript('python', ['src/articles/news.py', input]);
       promisses.push(
         new Promise<Article[]>((resolve, reject) => {
           process.stdout.on('data', data => {
@@ -21,8 +17,8 @@ export class ElenaService {
             resolve(JSON.parse(data.toString()));
           });
           process.stderr.on('data', data => {
-              console.log('python failed');
-              console.log(data.toString());
+            console.log('python failed');
+            console.log(data.toString());
             reject(JSON.parse(data.toString()));
           });
         }),
