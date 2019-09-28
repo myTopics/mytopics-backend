e78e713c6da4574458a706aa3e8041c751ae7c90
sebossert@students.zhaw.ch <sebossert@protonmail.com>
2019-09-28 09:50:22 +0200

    added article model structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesController } from './articles/articles.controller';
+import { ArticlesService } from './articles/articles.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ArticlesController],
+  providers: [AppService, ArticlesService],
 })
 export class AppModule {}
